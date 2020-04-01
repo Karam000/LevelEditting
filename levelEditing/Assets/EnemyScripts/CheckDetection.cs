5c33f51df5f8271e804d9e78ad357e149da897e5
Fatma Abdeen <fatmaabdeen63@gmail.com>
2020-04-01 18:23:37 +0200

    Enemies Scripts

---
+++ levelEditing/Assets/EnemyScripts/CheckDetection.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CheckDetection : MonoBehaviour
+{
+   
+    [SerializeField] GameObject _RestartPanel;
+    float TaregtTime = 5.0f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Founded");
+            TaregtTime -= Time.deltaTime;
+
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            TaregtTime -= Time.deltaTime;
+            if(TaregtTime < 0)
+            {
+                Debug.Log("Detected");
+                _RestartPanel.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("TriggerExit");
+            TaregtTime = 10.0f;
+        }
+    }
+    //void CheckingDetection()
+    //{
+        
+    //}
+}
