462486165c8066022409a418bcc4a82f177962f4
Mohamed Karam <mohamedkaram09@gmail.com>
2020-04-01 18:17:59 +0200

    Animator for player Movign player script animating player script

---
+++ levelEditing/Assets/Scripts/PlayerAnimation/AnimatePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatePlayer : MonoBehaviour
+{
+    Animator animator;
+    int walkIDanim = -1;
+    int idleIDanim = -1;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        walkIDanim = Animator.StringToHash("Walk");
+        idleIDanim = Animator.StringToHash("Idle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetBool(walkIDanim, true);
+            animator.SetBool(idleIDanim, false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            animator.SetBool(walkIDanim, false);
+            animator.SetBool(idleIDanim, true);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Rotate(Vector3.up, 180);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up, 90);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.up, -90);
+        }
+    }
+}
---
+++ levelEditing/Assets/Scripts/PlayerMovement/MovePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 3.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        moveDirection *= speed;
+
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
